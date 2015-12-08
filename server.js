dfec0884e1e3c147f8b2d19e3fd7a4ec63502c45
cerensavasan <cerensavasan@gmail.com>
2015-12-08 14:29:10 -0500

    Trying to deploy still

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var path = require('path');
-var fs = require('fs');
-var url = require('url');
 var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname)));
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 app.post('/', function (req, res) {
     console.log('sendfile get is working');
