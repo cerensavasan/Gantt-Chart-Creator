c47aed5cdf255b097bc2518a7ec72477497a3ebe
Adilet Issayev <adilet.issayev@gmail.com>
2015-12-11 15:01:08 -0500

    ajax started

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,13 +1,42 @@
 //generating table
 function populate(){
   console.log("inside populate");
-  //var divBy = document.getElemen
-  $('body').append('<div id="container" display="table">');
-  for(j = 0; j < 8; j++) {
-      $('body').append('<div id="row' + j + '" display="table-row">');
-      for(i = 0; i < 8; i++) {
-        $('body').append('<div class="square"></div>');
-      }
-  }
+  var divCal = document.getElementById('calendar');
+  
+  divCal.innerHTML = "Test";
+  
+  // $('body').append('<div id="container" display="table">');
+  // for(j = 0; j < 8; j++) {
+  //     $('body').append('<div id="row' + j + '" display="table-row">');
+  //     for(i = 0; i < 8; i++) {
+  //       $('body').append('<div class="square"></div>');
+  //     }
+  // }
   console.log("populating done");
   }
+  
+  
+function displayCalendar() {
+  var date1 = document.getElementById('start');
+  var date2 = document.getElementById('end');
+  
+  var req = new XMLHttpRequest();
+  
+  req.onreadystatechange = function() {
+    
+        if( req.readyState !== XMLHttpRequest.DONE )
+          return;
+
+        if(req.status === 200) {
+          //buildList(JSON.parse(req.responseText));
+          //el.value = "";
+          //mydiv.innerHTML = JSON.parse(req.responseText);
+        }
+      
+    }
+    
+    req.open('POST', '/calendar', true);
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    req.send('date1='+date1.value, 'date2='+date2.value);
+
+}
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.post('/', function (req, res) {
     console.log('sendfile get is working');
@@ -19,6 +24,15 @@ app.get('/dates', function (req, res) {
     // date 1 is this
     // date 2 is this    
     console.log('dates received, thanks');
+});
+
+
+app.post('/calendar', function (req, res) {
+    var d1 = req.body.date1;
+    var d2 = req.body.date2;
+    
+    console.log(d1);
+    console.log(d2);
 });
 
 
